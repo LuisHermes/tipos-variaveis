c656c98b3309bcdb1ba50d93fe68f2db525b29a2
luishermes <luishenriquefrance@gmail.com>
2023-09-09 16:28:29 -0300

    Erros e Debug

---
+++ src/Operadores.java
@@ -0,0 +1,27 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+          String nomeCompleto = "LUIS" + "HERMES";
+          
+          System.out.println(nomeCompleto);
+         
+    }
+
+    // classe Operadores.java
+    String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+    // qual o resultado das expressoes abaixo?
+    //String 
+    //concatenacao ="?";  // Aqui é para mostrar os Debug
+
+    //concatenacao=1+1+1+"1";
+
+    //concatenacao=1+"1"+1+1;
+
+    //concatenacao=1+"1"+1+"1";
+
+    //concatenacao="1"+1+1+1;
+
+    //concatenacao="1"+(1+1+1); 
+
+}
