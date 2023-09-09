2b768db76420a42f911baefee2091d9190a429fa
luishermes <luishenriquefrance@gmail.com>
2023-09-09 16:10:32 -0300

    Seguindo com o Curso

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,22 @@
+import javax.print.DocFlavor.STRING;
+
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+            String meuNome = "Luis";
+
+            double salarioMinino = 2500; //2.500 reais 
+
+            short numeroCurto = 1;
+            int numeroNormal = numeroCurto;
+            short numeroCurto2 = (short) numeroNormal;
+
+            int numero = 5;
+
+            numero = 10;
+
+            System.out.println(numero);
+            System.out.println(meuNome);
     }
 }
