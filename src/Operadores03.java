d27e2c972605a50c623c3e844dd9f13952419ff8
luishermes <luishenriquefrance@gmail.com>
2023-09-11 10:09:53 -0300

    Commit

---
+++ src/Operadores03.java
@@ -0,0 +1,16 @@
+public class Operadores03 {
+    
+    public static void main(String [] args ) {
+         int numero = 5;
+
+         numero = -numero;
+         
+         System.out.println(numero);
+
+         numero = numero * -1;
+
+         System.out.println(numero);
+
+         // Primeiro run negativo e segundo run positivo
+    }
+}
