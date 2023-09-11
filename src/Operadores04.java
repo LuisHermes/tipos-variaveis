813e2e265e76185e5df04cac543cf39bbd406cea
luishermes <luishenriquefrance@gmail.com>
2023-09-11 10:18:56 -0300

    True / False

---
+++ src/Operadores04.java
@@ -0,0 +1,12 @@
+public class Operadores04 {
+    public static void main(String [] args) {
+        
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+        // convertendo true para false "!"
+    }
+}
