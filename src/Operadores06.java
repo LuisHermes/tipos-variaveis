10d6c2793df3b421f42ee35edd194779216bd9c8
luishermes <luishenriquefrance@gmail.com>
2023-09-11 10:40:19 -0300

    Operadores Java

---
+++ src/Operadores06.java
@@ -0,0 +1,9 @@
+public class Operadores06 {
+    public static void main (String [] args) {
+
+        String nomeUm = "LUIS";
+        String nomeDois = "LUIS";
+
+        System.out.println(nomeUm.equals(nomeDois));
+    }
+}
---
+++ src/Operadores07.java
@@ -0,0 +1,11 @@
+public class Operadores07 {
+    public static void main (String [] args) {
+        boolean condicao1 = true;
+
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condiçoes são versadeiras ");
+        }
+    }
+}
