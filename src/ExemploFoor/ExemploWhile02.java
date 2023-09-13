a3ef1b588bbe5a73eec315bb46c766b4cba12d96
luishermes <luishenriquefrance@gmail.com>
2023-09-12 21:35:09 -0300

    Curso Java

---
+++ src/ExemploFoor/ExemploWhile02.java
@@ -0,0 +1,23 @@
+package ExemploFoor;
+
+
+import java.util.Random;
+
+public class ExemploWhile02 {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+        
+        do {
+            System.out.println("Telefone tocando");
+
+        }while(tocando());
+
+        System.out.println("Alô !!!");
+        }
+        private static boolean tocando () {
+            boolean atendeu = new Random().nextInt(3)==1;
+            System.out.println("Atendeu? " + atendeu);
+            return ! atendeu;
+        }
+    }
+
